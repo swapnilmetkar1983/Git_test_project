44e042d64f9dd28e2624d0f5d4257369bda7013c
Swapnil Metkar <swapnilmetkar83@gmail.com>
2023-03-23 13:24:14 +0530

    new classfile Math.cs is added

---
+++ Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_test_project
+{
+    class Math
+    {
+        int a, b,c;
+
+        public int AddNumbers(int a, int b)
+            { return a + b; }
+         
+    }
+}
